6f93d71bc527e63b7ae929d717d8e34e8ac1207c
sherlock1854 <sherlock5128@gmail.com>
2020-09-28 23:46:45 +0900

    ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::Base
+
+
+    def after_sign_in_path_for(resource)
+      case resource
+      when Admin
+        admin_top_path
+      when Customer
+          root_path
+      end
+    end
+
+
+    def after_sign_up_path_for(resource)
+      case resource
+      when Admin
+        admin_top_path
+      when Customer
+        root_path
+      end
+    end
+
+
+    def after_sign_out_path_for(resource)
+      if resource == :admin
+        new_admin_session_path
+      else
+        new_customer_session_path
+      end
+  end
+
+
 end
