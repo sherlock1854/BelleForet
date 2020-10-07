7a761d2cb325658069b79f0731499724029f2225
sherlock1854 <sherlock5128@gmail.com>
2020-10-08 00:01:25 +0900

    views,controllers,config

--- app/controllers/admin/topics_controller.rb
+++ app/controllers/admin/topics_controller.rb
@@ -16,22 +16,44 @@ class Admin::TopicsController < ApplicationController
 
 
     def show
+      @topic = Topic.find(params[:id])
     end
 
 
     def edit
+      @topic = Topic.find(params[:id])
     end
 
 
     def update
+      @topic = Topic.find(params[:id])
+      if @topic.update(topic_params)
+        flash[:notice] = "TOPICが変更されました。"
+        redirect_to admin_topic_path(@topic.id)
+      else
+        render "edit"
+      end
     end
 
 
     def create
+      @topic = Topic.new(topic_params)
+      @topic.admin_id = current_admin.id
+      @topics = Topic.all
+      if @topic.save!
+        flash[:notice] = "TOPICが保存されました。"
+        redirect_to admin_topic_path(@topic)
+      else
+        flash[:alert] = "再度入力してください。"
+        render 'index'
+      end
     end
 
 
     def destroy
+      @topic = Topic.find(params[:id])
+      @topic.destroy
+      redirect_to admin_topics_path
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
+  #admin
   devise_for :admins, controllers: {
-    sessions: 'admin/sessions'
+    sessions: 'admin/sessions',
+    passwords: "customer/devises/passwords",
+    registrations: "customer/devises/registrations"
   }
 
 
+  #customer
   devise_for :customers, controllers: {
-    sessions: 'customer/sessions'
+    sessions: 'customer/sessions',
+    passwords: "admin/devises/passwords",
+    registrations: "admin/devises/registrations"
   }
 
 
@@ -21,9 +27,20 @@ Rails.application.routes.draw do
   get 'homes/party' => 'customer/homes#party', as: 'party'
   get 'homes/wedding' => 'customer/homes#wedding', as: 'wedding'
 
+  scope module: :customer do
+    resources :topics, only: [:index, :show] do
+      resource :favorites, only: [:create, :destroy]
+      resources :comments, only: [:create, :destroy]
+    end
+  end
+
 
   #admin
   get 'admins/home/top' => 'admin/homes#top', as: 'admin_top'
+
+  namespace :admin do
+    resources :topics, only: [:new, :index, :show, :edit, :create, :update, :destroy]
+  end
 
 
 end
