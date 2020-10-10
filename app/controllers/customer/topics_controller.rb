9a0c0ebb086e4cadcb9c46ac87a8e0f566015f83
sherlock1854 <sherlock5128@gmail.com>
2020-10-11 00:02:38 +0900

    controllers,views

--- app/controllers/customer/topics_controller.rb
+++ app/controllers/customer/topics_controller.rb
@@ -1,2 +1,14 @@
 class Customer::TopicsController < ApplicationController
+
+
+    def index
+      @topic = Topic.new
+      @topics = Topic.all.order(created_at: :desc).page(params[:page])
+    end
+
+
+    def show
+    end
+
+
 end
